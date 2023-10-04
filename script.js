48ec5bfb051fcdbafb1e422583176ada15b8fc61
separk-1 <78338116+separk-1@users.noreply.github.com>
2023-10-05 01:10:38 +0900

    1

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 document.getElementById('menu-icon').addEventListener('click', function() {
     var navbar = document.getElementById('navbar');
-    if (navbar.style.maxHeight === '200px') {  /* 열려있는 상태 */
+    if (navbar.style.maxHeight === '200px') {
         navbar.style.maxHeight = '0';
     } else {
-        navbar.style.maxHeight = '200px';  /* 닫혀있는 상태 */
+        navbar.style.maxHeight = '200px';
     }
 });
