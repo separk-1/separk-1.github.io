761ea26baf38ee8528d9626ce752996cfcea54ae
separk-1 <78338116+separk-1@users.noreply.github.com>
2023-10-05 18:30:58 +0900

    1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ document.querySelectorAll('#navbar a').forEach(anchor => {
 
 document.querySelector('.menu-icon').addEventListener('click', function() {
     var navItems = document.querySelectorAll('nav li');
-    for (var i = 1; i < navItems.length; i++) {  // 첫 번째 항목(햄버거 메뉴)을 제외하고 순회
+    for (var i = 1; i < navItems.length; i++) {  
         if (navItems[i].style.display === 'block') {
             navItems[i].style.display = 'none';
         } else {
@@ -18,13 +18,12 @@ document.querySelector('.menu-icon').addEventListener('click', function() {
     }
 });
 
-
 document.addEventListener('DOMContentLoaded', function() {
     let isDragging = false;
     let rotationSpeed = 0;
     let rotation = 0;
     const image = document.getElementById('profileImage');
-    const friction = 0.95; // Friction for deceleration
+    const friction = 0.95; 
 
     image.addEventListener('mousedown', function(e) {
         if(e.target === image) {
@@ -35,7 +34,7 @@ document.addEventListener('DOMContentLoaded', function() {
     document.addEventListener('mousemove', function(e) {
         if (isDragging) {
             const mouseMove = e.movementX;
-            rotationSpeed = mouseMove * 0.5; // Adjust the multiplier for sensitivity
+            rotationSpeed = mouseMove * 0.5; 
         }
     });
 
@@ -59,29 +58,14 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
-    // 모든 .toggle-icon 요소를 선택합니다.
     const toggleIcons = document.querySelectorAll('.toggle-icon');
 
-    // 각 .toggle-icon 요소에 클릭 이벤트 리스너를 추가합니다.
     toggleIcons.forEach(icon => {
         icon.addEventListener('click', function () {
-            // 클릭된 .toggle-icon의 형제 요소인 .project-detail를 선택하고 토글합니다.
             const detail = this.nextElementSibling.nextElementSibling;
             detail.style.display = detail.style.display === 'none' ? 'block' : 'none';
-
-            // 클릭된 .toggle-icon의 클래스를 토글합니다.
             this.classList.toggle('fa-chevron-down');
             this.classList.toggle('fa-chevron-up');
-        });
-    });
-});
-
-
-document.addEventListener('DOMContentLoaded', (event) => {
-    document.querySelectorAll('.toggle-icon').forEach((toggleIcon) => {
-        toggleIcon.addEventListener('click', (event) => {
-            const projectDetail = event.target.parentElement.querySelector('.project-detail');
-            projectDetail.style.display = (projectDetail.style.display === 'none' || projectDetail.style.display === '') ? 'block' : 'none';
         });
     });
 });
