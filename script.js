5c44e1bcb53a917e5289d2838c8681c2d3d83cb2
separk-1 <78338116+separk-1@users.noreply.github.com>
2023-10-06 00:07:25 +0900

    1

--- script.js
+++ script.js
@@ -1,30 +1,34 @@
-document.querySelectorAll('#navbar a').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: 'smooth'
-        });
-    });
-});
-
-document.querySelector('.menu-icon').addEventListener('click', function() {
-    var navItems = document.querySelectorAll('nav li');
-    for (var i = 1; i < navItems.length; i++) {  
-        if (navItems[i].style.display === 'block') {
-            navItems[i].style.display = 'none';
-        } else {
-            navItems[i].style.display = 'block';
-        }
-    }
-});
-
 document.addEventListener('DOMContentLoaded', function() {
+    // Variables
     let isDragging = false;
     let rotationSpeed = 0;
     let rotation = 0;
     const image = document.getElementById('profileImage');
     const friction = 0.97;
 
+    // Navbar smooth scrolling
+    document.querySelectorAll('#navbar a').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+            e.preventDefault();
+            document.querySelector(this.getAttribute('href')).scrollIntoView({
+                behavior: 'smooth'
+            });
+        });
+    });
+
+    // Mobile menu toggle
+    document.querySelector('.menu-icon').addEventListener('click', function() {
+        var navItems = document.querySelectorAll('nav li');
+        for (var i = 1; i < navItems.length; i++) {  
+            if (navItems[i].style.display === 'block') {
+                navItems[i].style.display = 'none';
+            } else {
+                navItems[i].style.display = 'block';
+            }
+        }
+    });
+
+    // Image rotation
     image.addEventListener('mousedown', function(e) {
         if(e.target === image) {
             isDragging = true;
@@ -55,57 +59,45 @@ document.addEventListener('DOMContentLoaded', function() {
         requestAnimationFrame(animate);
     }
     animate();
-});
 
-
-document.addEventListener("DOMContentLoaded", function () {
+    // Project details toggle
     const toggleIcons = document.querySelectorAll('.toggle-icon');
-
     toggleIcons.forEach(icon => {
         icon.addEventListener('click', function () {
             const detail = this.nextElementSibling.nextElementSibling;
             detail.classList.toggle('open');
-
             this.classList.toggle('fa-chevron-down');
             this.classList.toggle('fa-chevron-up');
         });
     });
-});
 
-document.addEventListener('DOMContentLoaded', function() {
+    // Image link popup
     const imageLinks = document.querySelectorAll('.image-link');
-
     imageLinks.forEach(link => {
         link.addEventListener('click', function(e) {
             e.preventDefault();
             window.open(link.href, 'ImagePopup', 'width=600,height=600');
         });
     });
+
+    // Command input
+    document.getElementById('command-input').addEventListener('keydown', function(e) {
+        if (e.key === 'Enter') {
+            const command = e.target.value.trim();
+            const [action, value] = command.split(' ');
+
+            switch(action) {
+                case 'rotate':
+                    rotationSpeed = parseFloat(value);
+                    break;
+                case 'stop':
+                    rotationSpeed = 0;
+                    break;
+                default:
+                    console.log('Unknown command');
+            }
+
+            e.target.value = '';  // Clear the command input
+        }
+    });
 });
-
-document.getElementById('commandInput').addEventListener('keyup', function(e) {
-    if (e.key === 'Enter') {
-        executeCommand();
-    }
-});
-
-function executeCommand() {
-    const commandInput = document.getElementById('commandInput');
-    const command = commandInput.value;
-    const [action, value] = command.split(' ');
-
-    switch(action) {
-        case 'rotate':
-            rotateImage(Number(value));
-            break;
-        case 'stop':
-            stopRotation();
-            break;
-        // 추가적인 명령어들...
-        default:
-            alert('Unknown command');
-    }
-
-    // Clear the input
-    commandInput.value = '';
-}
