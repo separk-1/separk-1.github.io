28e794d1cf0a671e3368a5b4cc89bdb7eb1b7c99
separk-1 <78338116+separk-1@users.noreply.github.com>
2023-10-05 18:45:58 +0900

    1

--- script.js
+++ script.js
@@ -58,12 +58,17 @@ document.addEventListener('DOMContentLoaded', function() {
 });
 
 document.addEventListener("DOMContentLoaded", function () {
+    // 모든 .toggle-icon 요소를 선택합니다.
     const toggleIcons = document.querySelectorAll('.toggle-icon');
 
+    // 각 .toggle-icon 요소에 클릭 이벤트 리스너를 추가합니다.
     toggleIcons.forEach(icon => {
         icon.addEventListener('click', function () {
-            const detail = this.nextElementSibling.nextElementSibling;
-            detail.style.display = detail.style.display === 'none' ? 'block' : 'none';
+            // 클릭된 .toggle-icon의 부모 요소(li)의 다음 형제 요소(.project-detail)를 선택하고 토글합니다.
+            const detail = this.parentElement.querySelector('.project-detail');
+            detail.style.display = detail.style.display === 'block' ? 'none' : 'block';
+
+            // 클릭된 .toggle-icon의 클래스를 토글합니다.
             this.classList.toggle('fa-chevron-down');
             this.classList.toggle('fa-chevron-up');
         });
